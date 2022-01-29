bee53fd4f720a4f78e7325d827c28635a6d49a92
Moritz Busch <mail@moritz-busch.de>
2022-01-29 16:16:14 +0100

    Formatting

--- crawler/unsere-zeitung.py
+++ crawler/unsere-zeitung.py
@@ -16,13 +16,14 @@ def crawling(base_url, overview_url="https://www.unsere-zeitung.at/category/nach
     article_tag = overview_soup.find(name="div",
                                      class_="article-container")
     article_images = article_tag.find_all(name="div",
-    class_="featured-image")
-    article_urls = [image.find(name="a", href=True)["href"] for image in article_images]
+                                          class_="featured-image")
+    article_urls = [image.find(name="a", href=True)["href"]
+                    for image in article_images]
 
     for i, url in enumerate(article_urls):
         print(f"{i+1:0>2}/{len(article_urls)} Crawling {url}")
         crawl_site(url)
-    
+
     # Go onto the next page
     next_overview_url = overview_soup.find(
         name="a",
@@ -35,6 +36,7 @@ def crawling(base_url, overview_url="https://www.unsere-zeitung.at/category/nach
     if next_overview_url:
         print("Opening next page.")
         crawling(base_url, next_overview_url["href"])
+
 
 def main():
     base_url = "https://www.unsere-zeitung.at/"
