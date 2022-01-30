fd27be288a44ac5732fc8d2c62839b736f3c02e3
Moritz Busch <mail@moritz-busch.de>
2022-01-30 02:37:19 +0100

    Added parsing to brandeins.py

--- crawler/brandeins.py
+++ crawler/brandeins.py
@@ -1,5 +1,8 @@
 import utilities as utl
 import re
+from bs4 import BeautifulSoup
+from pathlib import Path
+import json
 
 
 def crawl_site(url, base_url):
@@ -8,7 +11,7 @@ def crawl_site(url, base_url):
     soup_main = soup.find(["main"])
 
     contains_easy_parts = bool(soup_main.find(
-        name="span", style="color: #ff0000;"))
+        name="span", style="color: #ff0000;") or soup_main.find(name="span", style="color: #800000;"))
 
     if contains_easy_parts:
         filepath = utl.get_crawled_path_from_url(url)
@@ -33,9 +36,105 @@ def crawling(base_url):
         crawl_site(url, base_url)
 
 
+def parse_soups(base_url: str):
+    """ Contrary to other webpages brandeins needs it's own parse_soup method
+    """
+    header = utl.load_header(base_url)
+    foldername, _ = utl.get_names_from_url(base_url)
+
+    print(utl.get_headerpath_from_url(base_url))
+    print(foldername)
+    removal = []
+
+    new_header = {}
+    for filename in header.keys():
+        # check if file has not been parsed
+        if not filename in header[filename]["matching_files"]:
+            new_header[filename] = header[filename]
+        else:
+            url = header[filename]["url"]
+            easy_filepath = utl.get_parsed_path_from_url(url + "_easy")
+            normal_filepath = utl.get_parsed_path_from_url(url + "_normal")
+
+            # save new header information
+            new_header[easy_filepath.stem] = {
+                "url": header[filename]["url"],
+                "crawl_date": header[filename]["crawl_date"],
+                "easy": True,
+                "publication_date": header[filename]["publication_date"],
+                "matching_files": [normal_filepath.stem]
+            }
+
+            new_header[normal_filepath.stem] = {
+                "url": header[filename]["url"],
+                "crawl_date": header[filename]["crawl_date"],
+                "easy": False,
+                "publication_date": header[filename]["publication_date"],
+                "matching_files": [easy_filepath.stem]
+            }
+
+            # remove from the header
+            # removal.append(filename)
+
+            # parse contents
+            soup = utl.read_soup(url)
+
+            texts = soup.find_all(
+                "section", attrs={"class": "textblock container"})
+
+            text_easy = []
+            text_normal = []
+            for text in texts:
+                paragraphs = text.find_all("p")
+                for paragraph in paragraphs:
+                    if paragraph.find("span", attrs={"style": "color: #ff0000;"}):
+                        text_easy.append(paragraph)
+                    else:
+                        text_normal.append(paragraph)
+
+            with open(easy_filepath, "w", encoding="utf-8") as f:
+                for paragraph in text_easy:
+                    text = paragraph.get_text()
+                    # clean up of text
+                    text = re.sub("\s+", " ", text)
+                    text = text.strip()
+                    # # remove empty lines
+                    if not text:
+                        continue
+                    for j, sentence in enumerate(re.split(r"([?.:!] )", text)):
+                        # print(f"{i}-{j}#{sentence}")
+                        # Move punctuation to the correct position
+                        if sentence in [". ", ": ", "? ", "! "]:
+                            f.seek(f.tell()-1)
+                        f.write(f"{sentence}\n")
+
+            with open(normal_filepath, "w", encoding="utf-8") as f:
+                for paragraph in text_normal:
+                    text = paragraph.get_text()
+                    # clean up of text
+                    text = re.sub("\s+", " ", text)
+                    text = text.strip()
+                    # # remove empty lines
+                    if not text:
+                        continue
+                    for j, sentence in enumerate(re.split(r"([?.:!] )", text)):
+                        # print(f"{i}-{j}#{sentence}")
+                        # Move punctuation to the correct position
+                        if sentence in [". ", ": ", "? ", "! "]:
+                            f.seek(f.tell()-1)
+                        f.write(f"{sentence}\n")
+
+    # for key in removal:
+    #     del header[key]
+    with open(Path(foldername, "header.json"), "w", encoding="utf-8") as f:
+        json.dump(new_header, f, indent=4)
+
+
 def main():
     base_url = "https://www.brandeins.de/"
     crawling(base_url)
+
+    parse_soups(base_url)
 
 
 if __name__ == '__main__':
