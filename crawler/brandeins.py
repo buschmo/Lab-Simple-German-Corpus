baf6e493ce5083af32f1cacc7098efeca918789a
Moritz Busch <mail@moritz-busch.de>
2022-03-01 03:05:53 +0100

    Fixed error in brandeins.py

--- crawler/brandeins.py
+++ crawler/brandeins.py
@@ -40,10 +40,16 @@ def crawling(base_url):
 def parse_soups(base_url: str):
     """ Contrary to other webpages brandeins needs it's own parse_soup method
     """
-    header = utl.load_header(base_url)
+    headerpath = utl.get_headerpath_from_url(base_url)
+    headerpath_old = Path(headerpath.parent, "header_old.json")
+    if not os.path.exists(headerpath_old):
+        header = utl.load_header(base_url)
+        with open(headerpath_old, "w", encoding="utf-8") as f:
+            json.dump(header, f, indent=4)
+    else:
+        with open(headerpath_old, "r") as f:
+            header = json.load(f)
     foldername, _ = utl.get_names_from_url(base_url)
-
-    removal = []
 
     output_folder = utl.get_parsed_path_from_url(base_url)
     if not os.path.exists(output_folder.parent):
@@ -51,91 +57,87 @@ def parse_soups(base_url: str):
 
     new_header = {}
     
-    
     for filename in header.keys():
+        # ignore special case
+        if filename == "www.brandeins.de__magazine__brand-eins-wirtschaftsmagazin__2019__qualitaet__ich-tu-mir-so-leid-deswegen-will-ich-jetzt-zaubern-lernen.html":
+            continue
+
         # check if file has not been parsed
-        if not filename in header[filename]["matching_files"]:
-            new_header[filename] = header[filename]
-        else:
-            url = header[filename]["url"]
-            easy_filepath = utl.get_parsed_path_from_url(url + "_easy")
-            normal_filepath = utl.get_parsed_path_from_url(url + "_normal")
+        url = header[filename]["url"]
+        easy_filepath = utl.get_parsed_path_from_url(url + "_easy")
+        normal_filepath = utl.get_parsed_path_from_url(url + "_normal")
 
-            # save new header information
-            new_header[easy_filepath.stem] = {
-                "url": header[filename]["url"],
-                "crawl_date": header[filename]["crawl_date"],
-                "easy": True,
-                "publication_date": header[filename]["publication_date"],
-                "matching_files": [normal_filepath.stem]
-            }
+        # save new header information
+        new_header[easy_filepath.stem] = {
+            "url": header[filename]["url"],
+            "crawl_date": header[filename]["crawl_date"],
+            "easy": True,
+            "publication_date": header[filename]["publication_date"],
+            "matching_files": [normal_filepath.stem]
+        }
 
-            new_header[normal_filepath.stem] = {
-                "url": header[filename]["url"],
-                "crawl_date": header[filename]["crawl_date"],
-                "easy": False,
-                "publication_date": header[filename]["publication_date"],
-                "matching_files": [easy_filepath.stem]
-            }
+        new_header[normal_filepath.stem] = {
+            "url": header[filename]["url"],
+            "crawl_date": header[filename]["crawl_date"],
+            "easy": False,
+            "publication_date": header[filename]["publication_date"],
+            "matching_files": [easy_filepath.stem]
+        }
 
-            # remove from the header
-            # removal.append(filename)
+        # parse contents
+        soup = utl.read_soup(url)
 
-            # parse contents
-            soup = utl.read_soup(url)
+        texts = soup.find_all(
+            "section", attrs={"class": "textblock container"})
 
-            texts = soup.find_all(
-                "section", attrs={"class": "textblock container"})
+        text_easy = []
+        text_normal = []
+        for text in texts:
+            paragraphs = text.find_all("p")
+            for paragraph in paragraphs:
+                if paragraph.find("span", attrs={"style": "color: #ff0000;"}):
+                    text_easy.append(paragraph)
+                else:
+                    text_normal.append(paragraph)
 
-            text_easy = []
-            text_normal = []
-            for text in texts:
-                paragraphs = text.find_all("p")
-                for paragraph in paragraphs:
-                    if paragraph.find("span", attrs={"style": "color: #ff0000;"}):
-                        text_easy.append(paragraph)
-                    else:
-                        text_normal.append(paragraph)
+        # empty texts are not saved
+        if (not text_easy) or (not text_normal):
+            continue
+        
+        with open(easy_filepath, "w", encoding="utf-8") as f:
+            for paragraph in text_easy:
+                text = paragraph.get_text()
+                # clean up of text
+                text = re.sub("\s+", " ", text)
+                text = text.strip()
+                # # remove empty lines
+                if not text:
+                    continue
+                for j, sentence in enumerate(re.split(r"([?.:!] )", text)):
+                    # print(f"{i}-{j}#{sentence}")
+                    # Move punctuation to the correct position
+                    if sentence in [". ", ": ", "? ", "! "]:
+                        f.seek(f.tell()-1)
+                    f.write(f"{sentence}\n")
 
-            with open(easy_filepath, "w", encoding="utf-8") as f:
-                for paragraph in text_easy:
-                    text = paragraph.get_text()
-                    # clean up of text
-                    text = re.sub("\s+", " ", text)
-                    text = text.strip()
-                    # # remove empty lines
-                    if not text:
-                        continue
-                    for j, sentence in enumerate(re.split(r"([?.:!] )", text)):
-                        # print(f"{i}-{j}#{sentence}")
-                        # Move punctuation to the correct position
-                        if sentence in [". ", ": ", "? ", "! "]:
-                            f.seek(f.tell()-1)
-                        f.write(f"{sentence}\n")
+        with open(normal_filepath, "w", encoding="utf-8") as f:
+            for paragraph in text_normal:
+                text = paragraph.get_text()
+                # clean up of text
+                text = re.sub("\s+", " ", text)
+                text = text.strip()
+                # # remove empty lines
+                if not text:
+                    continue
+                for j, sentence in enumerate(re.split(r"([?.:!] )", text)):
+                    # print(f"{i}-{j}#{sentence}")
+                    # Move punctuation to the correct position
+                    if sentence in [". ", ": ", "? ", "! "]:
+                        f.seek(f.tell()-1)
+                    f.write(f"{sentence}\n")
 
-            with open(normal_filepath, "w", encoding="utf-8") as f:
-                for paragraph in text_normal:
-                    text = paragraph.get_text()
-                    # clean up of text
-                    text = re.sub("\s+", " ", text)
-                    text = text.strip()
-                    # # remove empty lines
-                    if not text:
-                        continue
-                    for j, sentence in enumerate(re.split(r"([?.:!] )", text)):
-                        # print(f"{i}-{j}#{sentence}")
-                        # Move punctuation to the correct position
-                        if sentence in [". ", ": ", "? ", "! "]:
-                            f.seek(f.tell()-1)
-                        f.write(f"{sentence}\n")
-
-    # for key in removal:
-    #     del header[key]
     with open(utl.get_headerpath_from_url(base_url), "w", encoding="utf-8") as f:
         json.dump(new_header, f, indent=4)
-
-    # remove one specific entry as it uses an image https://www.brandeins.de/magazine/brand-eins-wirtschaftsmagazin/2019/gehalt/leichte-sprache-also-haben-wir-uns-nur-ganz-wenig-vorgenommen
-    utl.remove_header_entry(base_url, "www.brandeins.de__magazine__brand-eins-wirtschaftsmagazin__2019__gehalt__leichte-sprache-also-haben-wir-uns-nur-ganz-wenig-vorgenommen_easy.html")
 
 
 
