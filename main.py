4d92319a982a1ace3c7d5eb1181e96ed2fcb4051
Moritz Busch <mail@moritz-busch.de>
2022-04-18 01:52:30 +0200

    Added main.py for easy execution

---
+++ main.py
@@ -0,0 +1,11 @@
+import main_crawler
+import main_matching
+
+def main():
+    print("Downloading and parsing.")
+    main_crawler.main(from_archive=True)
+    print("Calculating results.\n\tThis might take a while and should not be aborted.\n\tThe calculation would start anew.")
+    main_matching.main()
+
+if __name__=="__main__":
+    main()
