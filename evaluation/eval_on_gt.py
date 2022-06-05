5160ac81764f54e046355f55ebda6b6128dacc02
vtoborek <vtoborek@uni-bonn.de>
2022-06-05 19:08:38 +0200

    changes to find out why some files are not found

--- evaluation/eval_on_gt.py
+++ evaluation/eval_on_gt.py
@@ -45,30 +45,39 @@ def create_gt_dict(simple_path, normal_path, nlp) -> dict:
     normal_sents = [str(s) for s in nlp(article).sents]
 
     # breaks here for certain articles, because spacy cannot reconstruct original sentences from result files
-    assert len(simple_sents)==len(normal_sents), f"Amount of German and simple German sentences doesn't match: " \
-                                                 f"Simple {len(simple_sents)} vs. German {len(normal_sents)} " \
-                                                 f"for {simple_path}"
+    # assert len(simple_sents)==len(normal_sents), f"Amount of German and simple German sentences doesn't match: " \
+    #                                              f"Simple {len(simple_sents)} vs. German {len(normal_sents)} " \
+    #                                              f"for {simple_path}"
 
     gt = {}
     for s, n in zip(simple_sents, normal_sents):
         gt[s] = n
-
     return gt
 
+not_found = 0
+nothing_correct = 0
 
 # for each article from samples
 for simple_article, article in samples:
     simple_article_name = os.path.split(simple_article)[1]
     article_name = os.path.split(article)[1]
 
+    # create ground truth dict: {simple sentence: normal sentence}
     article_gt = create_gt_dict(simple_article, article, nlp)
+
     # for each combination of sim_measure, matching_method, sd_threshold
     for sim, match_m, thres in product(sim_measures, matching_methods, sd_thresholds):
         name = utl.make_matching_path(simple_article_name, article_name, sim, match_m, thres)
         correct_alignments = 0
+        try:
+            with open(os.path.join(path_to_matchings, os.path.split(name)[1])) as f:
+                alignments = json.load(f)
+        # TODO: find out, why some files are not found under matches
+        except FileNotFoundError:
+            print(f">> FileNotFoundError: for article '{article_name}' with matching path '{name}'")
+            not_found += 1
+            continue
 
-        with open(os.path.join(path_to_matchings, os.path.split(name)[1])) as f:
-            alignments = json.load(f)
         for a in alignments:
             try:
                 if article_gt[a[1][0]] == a[1][1]:
@@ -76,11 +85,20 @@ for simple_article, article in samples:
             except KeyError:
                 pass
 
-        precision = correct_alignments / len(alignments)
-        recall = correct_alignments / len(article_gt)
-        F1 = 2 * (precision * recall) / (precision + recall)
+        if correct_alignments > 0:
+            precision = correct_alignments / len(alignments)
+            recall = correct_alignments / len(article_gt)
+            F1 = 2 * (precision * recall) / (precision + recall)
+        else:
+            print(f">>> No correct alignments found in {article} by {sim}-{match_m}-{thres}")
+            precision = 0
+            recall = 0
+            F1 = 0
+            nothing_correct += 1
 
-        results.append({"sim_measure": sim,
+        results.append({"website": article.split("/")[0],
+                        "article": article_name,
+                        "sim_measure": sim,
                         "matching_method": match_m,
                         "thres": thres,
                         "correct": correct_alignments,
@@ -89,4 +107,6 @@ for simple_article, article in samples:
                         "F1": F1})
 
 results_df = pd.DataFrame(results)
+print(f"Number of files not found: {not_found}")
+print(f"Number of instances with not a single correct alignment: {nothing_correct}")
 print(results_df)
